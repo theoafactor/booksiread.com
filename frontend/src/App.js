92809d6f3048f583a826f2b7a2979c78a73c0799
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2023-06-14 13:48:19 +0100

    About us

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,26 @@
-
-import logo from './logo.svg';
-import { Routes, Route } from 'react-router-dom';
-import Home from './Components/Home/Home';
-import Register from './Components/Register/Register';
-import Login from './Components/Login/Login';
-import User from "./Components/User/User";
-import './App.css';
-import { createContext } from 'react';
+import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Register from "./Components/Register/Register";
+import Login from "./Components/Login/Login";
+import About from "./Components/About/About";
+// import User from "./Components/User/User";
+import "./App.css";
 
 /**
- * - We need a way to centrally manage data/props. 
+ * - We need a way to centrally manage data/props.
  * For this, we will be using useContext
  */
 
-
 function App() {
-
-
-
-
-
-
   return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/user" element={<User />}></Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/about-us" element={<About />} />
+    </Routes>
   );
 }
 
 export default App;
-
