54f47e4e4f0e5a2343ebb4bfd023b6930face99c
Olu Adeyemo <theoafactor@gmail.com>
2024-04-25 19:10:24 +0100

    First update 2 for v2

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -4,7 +4,7 @@ function Home(){
 
     return <>
                 <Navbar></Navbar>
-                <div className="jumbotron bg-danger">
+                <div className="jumbotron bg-primary">
                     <h3>Books I Love to Read &gt; <small>A curated list of books all of us are currently reading ...</small></h3>
                     <hr></hr>
                     <p>
